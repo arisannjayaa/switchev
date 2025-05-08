67692f280d3739e5bfecc96b43e59883ae2c5a60
WAYAN\wayan <wayanarisanjaya01@gmail.com>
2025-05-09 01:16:12 +0800

    asset build

--- public/build/assets/app-P4Vjmj2s.js
+++
@@ -1,1 +0,0 @@
-import{l as a,h as d}from"./helper-BjVJ0fku.js";$("body").append(a());d(1e3);
---
+++ public/build/assets/spu-CpSbTnTN.js
@@ -0,0 +1,263 @@
+import{c as s,S as m,d as p,n as y,a as t}from"./helper-DPa9qwI8.js";document.addEventListener("DOMContentLoaded",function(){function c(){return`
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_rem" value="Uji rem">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_rem" value="${t(89e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_rem" id="vol_uji_rem" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_rem" id="total_uji_rem">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_lampu_utama" value="Uji lampu utama">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_lampu_utama" value="${t(765e3,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_lampu_utama" id="vol_uji_lampu_utama" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_lampu_utama" id="total_uji_lampu_utama">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_speedometer" value="Uji speedometer">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_speedometer" value="${t(1e6,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_speedometer" id="vol_uji_speedometer" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_speedometer" id="total_uji_speedometer">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_kontruksi" value="Pemeriksaan konstruksi">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_kontruksi" value="${t(445e3,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_kontruksi" id="vol_uji_kontruksi" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_kontruksi" id="total_uji_kontruksi">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_klakson" value="Uji klakson">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_klakson" value="${t(71e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_klakson" id="vol_uji_klakson" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_klakson" id="total_uji_klakson">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_berat" value="Pengukuran berat kendaraan bermotor">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_berat" value="${t(43e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_berat" id="vol_uji_berat" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_berat" id="total_uji_berat">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_dimensi" value="Pengukuran dimensi">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_dimensi" value="${t(66e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_dimensi" id="vol_uji_dimensi" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_dimensi" id="total_uji_dimensi">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_kebisingan" value="Uji Kebisingan R 41">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_kebisingan" value="${t(3e6,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_kebisingan" id="vol_uji_kebisingan" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_kebisingan" id="total_uji_kebisingan">
+                </td>
+            </tr>
+        `}function _(){return`
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_rem" value="Uji rem">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_rem" value="${t(197e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_rem" id="vol_uji_rem" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_rem" id="total_uji_rem">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_lampu_utama" value="Uji lampu utama">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_lampu_utama" value="${t(105e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_lampu_utama" id="vol_uji_lampu_utama" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_lampu_utama" id="total_uji_lampu_utama">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_radius_putar" value="Uji lampu utama">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_radius_putar" value="${t(5e5,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_radius_putar" id="vol_uji_radius_putar" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_radius_putar" id="total_uji_radius_putar">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_klakson" value="Uji klakson">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_klakson" value="${t(106e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_klakson" id="vol_uji_klakson" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_klakson" id="total_uji_klakson">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_side_slip" value="Uji kincup roda (side slip)">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_side_slip" value="${t(105e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_side_slip" id="vol_uji_side_slip" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_side_slip" id="total_uji_side_slip">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_berat" value="Pengukuran berat kendaraan bermotor">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_berat" value="${t(87e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_berat" id="vol_uji_berat" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_berat" id="total_uji_berat">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_dimensi" value="Pengukuran dimensi">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_dimensi" value="${t(132e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_dimensi" id="vol_uji_dimensi" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_dimensi" id="total_uji_dimensi">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_speedometer" value="Uji speedometer">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_speedometer" value="${t(304e4,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_speedometer" id="vol_uji_speedometer" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_speedometer" id="total_uji_speedometer">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_kontruksi" value="Pemeriksaan konstruksi">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_kontruksi" value="${t(37e5,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_kontruksi" id="vol_uji_kontruksi" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_kontruksi" id="total_uji_kontruksi">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input" name="name_uji_kebisingan" value="Uji Kebisingan R 51">
+                </td>
+                <td>
+                    <input type="text" readonly class="form-control fixed-width-input convert-currency" name="amount_uji_kebisingan" value="${t(7e6,"IDR",0)}">
+                </td>
+                <td>
+                    <input type="number" class="form-control fixed-width-input" name="vol_uji_kebisingan" id="vol_uji_kebisingan" value="0">
+                </td>
+                <td>
+                    <input type="text" class="form-control fixed-width-input" readonly value="${t(0,"IDR",0)}" name="total_uji_kebisingan" id="total_uji_kebisingan">
+                </td>
+            </tr>
+        `}function f(o){let i=[];o==="Sepeda Motor"&&(i=[{name:"uji_rem",amount:89e4},{name:"uji_lampu_utama",amount:765e3},{name:"uji_speedometer",amount:1e6},{name:"uji_kontruksi",amount:445e3},{name:"uji_klakson",amount:71e4},{name:"uji_berat",amount:43e4},{name:"uji_dimensi",amount:66e4},{name:"uji_kebisingan",amount:3e6}]),o==="Selain Sepeda Motor"&&(i=[{name:"uji_rem",amount:197e4},{name:"uji_lampu_utama",amount:105e4},{name:"uji_radius_putar",amount:5e5},{name:"uji_speedometer",amount:304e4},{name:"uji_klakson",amount:106e4},{name:"uji_side_slip",amount:105e4},{name:"uji_berat",amount:87e4},{name:"uji_dimensi",amount:132e4},{name:"uji_kontruksi",amount:37e5},{name:"uji_kebisingan",amount:7e6}]),i.forEach(a=>{const n=(parseFloat($(`#vol_${a.name}`).val())||0)*a.amount,e=t(n,"IDR",0);$(`#total_${a.name}`).val(e)})}$("#type").on("change",function(){const o=$(this).val(),i=$("#table-amount tbody");i.empty(),o==="Sepeda Motor"?i.append(c()):o==="Selain Sepeda Motor"?i.append(_()):i.empty(),$(document).on("input",'input[name^="vol_"]',function(){f(o)})}),$("#form-generate-spu").submit(function(o){o.preventDefault(),$("#id").val();let i=new FormData(this),a="#btn-submit",d=$("#generate-spu-url").val();$(a).empty().append(`<div class="spinner-border" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                                </div>`).prop("disabled",!0),fetch(d,{method:"POST",headers:{"X-CSRF-TOKEN":s()},body:i}).then(n=>n.json()).then(n=>{if(n.code==200&&fetch(n.data.download).then(e=>e.blob()).then(e=>{const r=window.URL.createObjectURL(e),u=document.createElement("a");u.href=r,u.download=n.data.file_name,document.body.appendChild(u),u.click(),window.URL.revokeObjectURL(r)}),n.code==403&&(m.fire({html:`<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-danger"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path><path d="M12 8v4"></path><path d="M12 16h.01"></path></svg>
+                            <h3>Terjadi Kesalahan</h3>
+        <div>${n.message}</div>`,confirmButtonText:"Ok",confirmButtonColor:"#d63939",customClass:{confirmButton:"btn btn-success w-100"}}),$(a).empty().append(n.data.button).prop("disabled",!1)),n.errors||n.invalid){new p(n.errors||n.invalid);let e=!1;$.each(n.errors,function(r){const u=$(`[name="${r}"]`);if(u.length&&!e){const l=$(".row-cards"),v=u.offset().top-l.offset().top-100;l.animate({scrollTop:l.scrollTop()+v},500),u.focus(),e=!0}}),$(a).empty().append("Generate Surat Pengantar Uji").prop("disabled",!1)}}).catch(n=>{console.log("Error:",n)}).finally(()=>{$(a).empty().append("Generate Surat Pengantar Uji").prop("disabled",!1)})}),$("#btn-send-spu-submit").click(function(o){o.preventDefault();let i=$("#id").val(),a=new FormData;a.append("id",i),a.append("spu_attachment",$("#spu_attachment")[0].files[0]);let d="#btn-send-spu-submit",n=$("#send-spu-url").val();$(d).empty().append(`<div class="spinner-border" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                                </div>`).prop("disabled",!0),fetch(n,{method:"POST",headers:{"X-CSRF-TOKEN":s()},body:a}).then(e=>e.json()).then(e=>{e.code==200&&(y(e.message),window.location.href=e.data.redirect),e.code==403&&(m.fire({html:`<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-danger"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path><path d="M12 8v4"></path><path d="M12 16h.01"></path></svg>
+                            <h3>Terjadi Kesalahan</h3>
+        <div>${e.message}</div>`,confirmButtonText:"Ok",confirmButtonColor:"#d63939",customClass:{confirmButton:"btn btn-success w-100"}}),$(d).empty().append(e.data.button).prop("disabled",!1)),(e.errors||e.invalid)&&(new p(e.errors||e.invalid),$(d).empty().append("Kirim").prop("disabled",!1))}).catch(e=>{console.log("Error:",e)}).finally(()=>{$(d).empty().append("Kirim").prop("disabled",!1)})}),$("#btn-send-spu").click(function(){$(".modal-title").html("Kirim Surat Pengantar Uji"),$("#modal-send-spu").modal("show")})});
