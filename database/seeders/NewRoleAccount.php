fd59d779ead005ac6cb71318526cde2aaac37e6f
WAYAN\wayan <wayanarisanjaya01@gmail.com>
2025-06-05 22:27:18 +0800

    new account

---
+++ database/seeders/NewRoleAccount.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class NewRoleAccount extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Administrator',
+            'email' => 'bpljskb@mail.com',
+            'password' => bcrypt('password1'),
+            'role_id' => Role::BPLJSKB,
+            'status' => 'verified'
+        ]);
+    }
+}
